504b594f249c67760280b7a7d4826065f6b350af
Bart Verhagen <bart@kapernikov.com>
2022-01-05 12:12:41 +0100

    New: Usr: Remapped escape to kj

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,1 +1,3 @@
 vim.api.nvim_set_keymap("n", "<space>", '<cmd>let @/ = ""<Cr>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("i", "kj", '<escape>', { noremap = true, silent = true })
